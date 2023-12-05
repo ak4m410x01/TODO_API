1be21355eae6e008aecf90a294ad888841eaa0ae
ak4m410x01 <abdallah10kamal@gmail.com>
2023-12-05 22:49:06 +0200

    implement pagination in core settings

--- todo/settings.py
+++ todo/settings.py
@@ -48,11 +48,16 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
+    # json rendering
     "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
     "DEFAULT_PARSER_CLASSES": ("rest_framework.parsers.JSONParser",),
+    # auth
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.TokenAuthentication",
     ),
+    # pagination
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
 
 ROOT_URLCONF = "todo.urls"
